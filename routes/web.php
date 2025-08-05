40f0b748b521c91e8be2a510a89cbe4b4af88235
khanakhanaputu <putukhana38@gmail.com>
2025-08-05 22:36:22 +0800

    add login with google

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,33 @@
 <?php
 
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('index');
 });
-
 Route::get('/dashboard', function () {
     return view('dashboard');
+});
+
+Route::get('/auth/redirect/google', function () {
+    return Socialite::driver('google')->redirect();
+});
+
+Route::get('/auth/google/callback', function () {
+    $googleUser = Socialite::driver('google')->user();
+    $avatar =  $googleUser->getAvatar();
+    $user = User::updateOrCreate([
+        'email' => $googleUser->getEmail(),
+    ], [
+        'name' => $googleUser->getName(),
+        'google_id' => $googleUser->getId(),
+        'avatar' => $avatar,
+    ]);
+
+    Auth::login($user);
+
+    return redirect('/dashboard'); // atau kemana aja
 });
